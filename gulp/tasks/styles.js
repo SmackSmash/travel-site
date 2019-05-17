ab26e02ca122de4d824bd5485eb9527f180b0563
Dan Moffat <danmoffat@gmail.com>
2019-05-17 23:08:58 +0100

    Done a lot of styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,13 +3,14 @@ const gulp = require('gulp'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
-  cssImport = require('postcss-import');
+  cssImport = require('postcss-import'),
+  mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
   console.log('CSS updated');
   return gulp
     .src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, autoprefixer, cssvars, nested]))
+    .pipe(postcss([cssImport, mixins, autoprefixer, cssvars, nested]))
     .on('error', error => {
       console.error(`${error.name}: ${error.reason}`);
     })
