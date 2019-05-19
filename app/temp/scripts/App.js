53c79b83826b160d468e65b7d28797daf1781377
Dan Moffat <danmoffat@gmail.com>
2019-05-19 22:02:41 +0100

    Completed hamburger icon with animations

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -72,6 +72,7 @@
 	  function MobileMenu() {
 	    _classCallCheck(this, MobileMenu);
 
+	    this.siteHeader = document.querySelector('.site-header');
 	    this.menuIcon = document.querySelector('.site-header__menu-icon');
 	    this.menuContent = document.querySelector('.site-header__menu-content');
 	    this.events();
@@ -86,6 +87,8 @@
 	    key: 'toggleMenu',
 	    value: function toggleMenu() {
 	      this.menuContent.classList.toggle('site-header__menu-content--is-visible');
+	      this.siteHeader.classList.toggle('site-header--is-expanded');
+	      this.menuIcon.classList.toggle('site-header__menu-icon--close-x');
 	    }
 	  }]);
 
